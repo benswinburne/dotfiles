e58ebfd007fd7f221e6fb0b8855d5ef0577f667b
benswinburne <ben.swinburne@gmail.com>
2025-11-29 18:57:04 -0500

    fix: manually load theme so its ready

---
+++ .config/nvim/lua/plugins/themes.lua
@@ -0,0 +1,27 @@
+-- https://smarttech101.com/how-to-configure-colorscheme-in-vim-neovim
+return {
+	{
+		"folke/tokyonight.nvim",
+	},
+	{
+		"neanias/everforest-nvim",
+	},
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+		config = function()
+			require("catppuccin").setup({
+				flavour = "frappe", -- latte, frappe, macchiato, mocha
+				term_colors = true,
+				transparent_background = false,
+			})
+
+			vim.opt.termguicolors = true
+			vim.cmd.colorscheme("catppuccin-frappe")
+		end,
+	},
+	{
+		"RRethy/base16-nvim",
+	},
+}

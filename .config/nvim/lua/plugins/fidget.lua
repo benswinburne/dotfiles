2ed6a39bcb134d4d378cc5f0ccbd71dd73573526
benswinburne <ben.swinburne@gmail.com>
2025-11-29 19:05:49 -0500

    feat: add fidget for lsp info

---
+++ .config/nvim/lua/plugins/fidget.lua
@@ -0,0 +1,19 @@
+return {
+	"j-hui/fidget.nvim",
+	opts = {
+		progress = {
+			ignore_done_already = true,
+			ignore_empty_message = true,
+			display = {
+				done_ttl = 0,
+			},
+		},
+		notification = {
+			window = {
+				winblend = 0, -- Background color opacity in the notification window
+				x_padding = 0, -- Padding from right edge of window boundary
+				y_padding = 2, -- Padding from bottom edge of window boundary
+			},
+		},
+	},
+}

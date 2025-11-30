e39a23aa5567f2893dc5b624ac42f53dbcdbb2cd
benswinburne <ben.swinburne@gmail.com>
2025-11-29 19:01:48 -0500

    feat: neotree behaviour tweaks

--- .config/nvim/lua/plugins/neotree.lua
+++ .config/nvim/lua/plugins/neotree.lua
@@ -9,12 +9,24 @@ return {
 	config = function()
 		local neotree = require("neo-tree")
 		local neotree_command = require("neo-tree.command")
+		local neotree_fs_command = require("neo-tree.sources.filesystem.commands")
 
 		neotree.setup({
 			close_if_last_window = true,
 			enable_git_status = true,
 			window = {
 				position = "right",
+				mappings = {
+					["e"] = "open_nofocus",
+				},
+			},
+			commands = {
+				open_nofocus = function(state)
+					neotree_fs_command.open(state)
+					vim.schedule(function()
+						vim.cmd([[Neotree focus]])
+					end)
+				end,
 			},
 			follow_current_file = {
 				-- This will find and focus the file in the active buffer every time
@@ -22,10 +34,28 @@ return {
 				enabled = false,
 				leave_dirs_open = false,
 			},
+			filesystem = {
+				filtered_items = {
+					visible = true,
+					hide_gitignored = true,
+				},
+				follow_current_file = {
+					enabled = true, -- This will find and focus the file in the active buffer every time
+					leave_dirs_open = true, -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
+				},
+			},
 		})
 
 		vim.keymap.set("n", "<leader>nt", function()
-			neotree_command.execute({ action = "show", toggle = true })
+			neotree_command.execute({ action = "focus", toggle = true })
 		end, { desc = "Toggle NeoTree", silent = true })
+
+		-- Ensure the neotree buffer is not left lingering when closing vim with
+		-- commands like :q instead of :qa
+		vim.api.nvim_create_autocmd("QuitPre", {
+			callback = function()
+				neotree_command.execute({ action = "close" })
+			end,
+		})
 	end,
 }
